795c25557eda090f1166272f998b4a12e6191968
unknown <cindy.mauricioe@gmail.com>
2026-06-03 13:49:20 -0300

    debug enviar pedido

--- api.php
+++ api.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1); error_reporting(E_ALL);
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
